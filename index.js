51076c173420e08dad67bd7b3d6bd878fae508b2
samer <samervalente@gmail.com>
2022-06-28 17:22:25 -0300

    feat: get user movements and balance

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient, ObjectId } from "mongodb";
 import jwt from "jsonwebtoken";
-import joi from "joi";
 import Joi from "joi";
+import dayjs from "dayjs"
+
 
 dotenv.config();
 
@@ -18,17 +19,19 @@ client.connect().then(() => {
   db = client.db("walletdb");
 });
 
+let date = `${dayjs().$D}/${dayjs().$M+1}`
+
 server.post("/register", async (req, res) => {
   const RegisterData = req.body;
 
-  const RegisterSchema = joi.object().keys({
+  const RegisterSchema = Joi.object().keys({
     name: Joi.string()
       .pattern(/^[a-z]+$/i)
       .required(),
     email: Joi.string()
       .pattern(/^[a-z0-9.!#]{4,30}@[a-z0-9]{3,15}(.[a-z]{1,5}){1,5}$/)
       .required(),
-    password: Joi.string().required(),
+    password: Joi.string().alphanum().required(),
   });
 
   const ResultValidate = RegisterSchema.validate(RegisterData);
@@ -57,11 +60,11 @@ server.post("/register", async (req, res) => {
 
 server.post("/login", async (req, res) => {
     const LoginData = req.body
-    const LoginSchema = joi.object().keys({
+    const LoginSchema = Joi.object().keys({
         email: Joi.string()
          .pattern(/^[a-z0-9.!#]{4,30}@[a-z0-9]{3,15}(.[a-z]{1,5}){1,5}$/)
          .required(),
-        password: Joi.string().required()
+        password: Joi.string().alphanum().required()
     })
     const ResultValidate = LoginSchema.validate(LoginData)
     if(ResultValidate.error){
@@ -79,11 +82,126 @@ server.post("/login", async (req, res) => {
         }
 
         const token = jwt.sign({id: user._id},'secret')
+       
         res.status(200).send({...LoginData, id:user._id, token})
 
     } catch (error) {
         
     }
+})
+
+server.get("/movements", async (req,res) => {
+    const token = req.headers.authorization.replace("bearer ","")
+    const {id} = jwt.verify(token,"secret")
+
+    if(!token || !id){
+        return res.sendStatus(400)
+    }
+   
+    try {
+        const UserMovements = await db.collection("movements").find({idMovement:id}).toArray()
+        res.send(UserMovements)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+})
+
+server.post("/entry", async (req, res) => {
+    const EntryData = req.body
+    
+    const token = req.headers.authorization.replace("bearer ","")
+    const {id} = jwt.verify(token,"secret")
+
+    if(!token || !id){
+        return res.sendStatus(400)
+    }
+
+    const EntryValidate = Joi.object().keys({
+        value:Joi.number().integer().required(),
+        description:Joi.string().required()
+    })
+
+    const ResultValidate = EntryValidate.validate(EntryData)
+    if(ResultValidate.error){
+        return res.sendStatus(422)
+    }
+
+    try {
+        await db.collection("movements").insertOne({
+            ...EntryData, date, type:"entry", idMovement:id 
+        })
+    } catch (error) {
+       return res.sendStatus(500)
+    }
+   
+    res.sendStatus(201)
+     
+})
+
+server.post("/exit", async (req, res) => {
+    const ExitData = req.body
+    
+    const token = req.headers.authorization.replace("bearer ","")
+    const {id} = jwt.verify(token,"secret")
+
+    if(!token || !id){
+        return res.sendStatus(400)
+    }
+
+    const ExitValidate = Joi.object().keys({
+        value:Joi.number().integer().required(),
+        description:Joi.string().required()
+    })
+
+    const ResultValidate = ExitValidate.validate(ExitData)
+    if(ResultValidate.error){
+        return res.sendStatus(422)
+    }
+
+    try {
+        await db.collection("movements").insertOne({
+            ...ExitData, date, type:"exit", idMovement:id 
+        })
+    } catch (error) {
+       return res.sendStatus(500)
+    }
+   
+    res.send(201)
+})
+
+
+server.get("/balance", async (req, res) => {
+    const token = req.headers.authorization.replace("bearer ","")
+    const {id} = jwt.verify(token,"secret")
+
+    if(!token || !id){
+        return res.sendStatus(400)
+    }
+
+    try {
+       const movementsUser = await db.collection("movements").find({idMovement:id}).toArray()
+       let entrySum = 0;
+       let exitSum = 0;
+       let balance = 0;
+       movementsUser.map(mov => {
+        if(mov.type === "entry"){
+            entrySum += Number(mov.value)
+        } 
+        if(mov.type === "exit"){
+            exitSum += Number(mov.value)
+        }
+       })
+         
+       balance = entrySum - exitSum
+
+       res.send({balance})
+
+    } catch (error) {
+        res.sendStatus(500)
+    }
+
+
+
 })
 
 
